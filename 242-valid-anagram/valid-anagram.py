058860d7a1336193b075aa6cc76d881251232311
Thirumoorthy369 <mr.thirumoorthys@gmail.com>
2025-04-27 18:07:23 +0530

    Time: 3 ms (98.40%) | Memory: 12.6 MB (67.63%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,5 +5,11 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False
+        l = "abcdefghijklmnopqrstuvwxyz"
+        for c in l:
+            if s.count(c) != t.count(c):
+                return False
+        return True
         
