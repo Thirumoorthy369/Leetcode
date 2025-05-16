7ba604994c8295a57f6458f0f81a92946dd68a56
Thirumoorthy369 <mr.thirumoorthys@gmail.com>
2025-05-16 22:25:27 +0530

    Time: 19 ms (38.73%) | Memory: 72.9 MB (43.22%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,8 +3,8 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-//    const s = new Set(nums);
-//    return s.size !== nums.length;
+   const s = new Set(nums);
+   return s.size !== nums.length;
 
-    return nums.length !== new Set(nums).size;
+    //return nums.length !== new Set(nums).size;
 };
