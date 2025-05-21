f0b2c897e6c48f9833d3a08553efe2b8be8f1ee0
Thirumoorthy369 <mr.thirumoorthys@gmail.com>
2025-05-21 06:19:41 +0530

    Time: 2 ms (18.74%) | Memory: 45.5 MB (93.05%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,58 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        boolean firstRowHasZero = false;
+        boolean firstColHasZero = false;
+
+        // Check if first column has zero
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) {
+                firstColHasZero = true;
+                break;
+            }
+        }
+
+        // Check if first row has zero
+        for (int j = 0; j < cols; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowHasZero = true;
+                break;
+            }
+        }
+
+        // Use first row and column as markers
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Apply markers to the rest of the matrix
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Zero out the first row if needed
+        if (firstRowHasZero) {
+            for (int j = 0; j < cols; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Zero out the first column if needed
+        if (firstColHasZero) {
+            for (int i = 0; i < rows; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
