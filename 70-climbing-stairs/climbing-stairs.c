d59698bb5c8cd37153d18eaf2e4a6cb06c5a16a0
Thirumoorthy369 <mr.thirumoorthys@gmail.com>
2025-05-22 16:43:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (63.25%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.c
@@ -0,0 +1,11 @@
+int climbStairs(int n) {
+    int prev=1, curr=1, next =1, i=2;
+    while(i <= n){
+
+        next = prev + curr;
+        prev = curr;
+        curr = next;
+        i = i+1;
+    }
+    return next;
+}
