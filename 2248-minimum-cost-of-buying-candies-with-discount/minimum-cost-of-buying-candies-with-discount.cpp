57eb117551e94995672eda190edcc2cfe7a0ccfc
Thirumoorthy369 <mr.thirumoorthys@gmail.com>
2025-05-23 17:22:23 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (56.20%) - LeetSync

--- 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.cpp
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.cpp
@@ -1,19 +1,39 @@
+// class Solution {
+// public:
+//     int minimumCost(vector<int>& cost) {
+//         int res =0;
+//         int n = cost.size();
+//         sort(cost.begin(), cost.end());
+        
+//           for (int i = n - 1; i >= 0; i--) {
+           
+//             if ((n - 1 - i) % 3 == 2) {
+//                 continue; 
+//             }
+//             res += cost[i];
+//           }
+        
+//         return res;
+//     }
+    
+// };
+
 class Solution {
 public:
     int minimumCost(vector<int>& cost) {
-        int res =0;
-        int n = cost.size();
-        sort(cost.begin(), cost.end());
+        sort(begin(cost), end(cost), greater<int>());
         
-          for (int i = n - 1; i >= 0; i--) {
-           
-            if ((n - 1 - i) % 3 == 2) {
-                continue; 
-            }
-            res += cost[i];
-          }
+        int i = 0, n = cost.size();
         
+        for (int i = 0; i < n / 3; i++) {
+            cost[3 * (i + 1) - 1] = 0;
+        }
+
+        int res = 0;
+        for (int n : cost) {
+            res += n;
+        }
+
         return res;
     }
-    
 };
