a5ccf67dfb86f0ded682729ae86da7f90dd1aeda
Thirumoorthy369 <mr.thirumoorthys@gmail.com>
2025-05-25 14:21:45 +0530

    Time: 1221 ms (54.82%) | Memory: 55.6 MB (90.89%) - LeetSync

--- 336-palindrome-pairs/palindrome-pairs.java
+++ 336-palindrome-pairs/palindrome-pairs.java
@@ -1,27 +1,45 @@
 class Solution {
     public List<List<Integer>> palindromePairs(String[] words) {
-    List<List<Integer>> pairs = new LinkedList<>();
-    if (words == null) return pairs;
-    HashMap<String, Integer> map = new HashMap<>();
-    for (int i = 0; i < words.length; ++ i) map.put(words[i], i);
-    for (int i = 0; i < words.length; ++ i) {
-        int l = 0, r = 0;
-        while (l <= r) {
-            String s = words[i].substring(l, r);
-            Integer j = map.get(new StringBuilder(s).reverse().toString());
-            if (j != null && i != j && isPalindrome(words[i].substring(l == 0 ? r : 0, l == 0 ? words[i].length() : l)))
-                pairs.add(Arrays.asList(l == 0 ? new Integer[]{i, j} : new Integer[]{j, i}));
-            if (r < words[i].length()) ++r;
-            else ++l;
-        }
-    }
-    return pairs;
-}
+        List<List<Integer>> result = new ArrayList<>();
+        if (words == null || words.length < 2) return result;
 
-private boolean isPalindrome(String s) {
-    for (int i = 0; i < s.length()/2; ++ i)
-        if (s.charAt(i) != s.charAt(s.length()-1-i))
-            return false;
-    return true;
-}
+        Map<String, Integer> reversedWordMap = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+            reversedWordMap.put(new StringBuilder(words[i]).reverse().toString(), i);
+        }
+
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            int len = word.length();
+
+            for (int j = 0; j <= len; j++) {
+                String prefix = word.substring(0, j);
+                String suffix = word.substring(j);
+
+                if (isPalindrome(prefix)) {
+                    Integer match = reversedWordMap.get(suffix);
+                    if (match != null && match != i) {
+                        result.add(Arrays.asList(match, i));
+                    }
+                }
+
+            
+                if (j != len && isPalindrome(suffix)) {
+                    Integer match = reversedWordMap.get(prefix);
+                    if (match != null && match != i) {
+                        result.add(Arrays.asList(i, match));
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    private boolean isPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            if (s.charAt(l++) != s.charAt(r--)) return false;
+        }
+        return true;
+    }
 }
