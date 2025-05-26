8ee1e715a533237c677b28c860d90c9d8354153f
Thirumoorthy369 <mr.thirumoorthys@gmail.com>
2025-05-26 17:02:27 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (81.44%) - LeetSync

---
+++ 2585-delete-greatest-value-in-each-row/delete-greatest-value-in-each-row.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int deleteGreatestValue(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int ans = 0;
+        for (int i = 0; i < m; i++){
+            sort(grid[i].begin(), grid[i].end());
+        }
+        
+        for (int j = 0; j < n; j++){
+            int maxi = INT_MIN;
+            for (int i = 0; i < m; i++){
+                maxi = max(maxi, grid[i][j]);
+            }
+            ans += maxi;
+        }
+        return ans;
+
+        // vector<vector<int>>dp(m, vector<int>(n-1));
+        // return dpms(o,o,m,n,dp);
+    }
+};
