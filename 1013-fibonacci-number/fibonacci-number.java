e292d077d1517fb60c9a40301d39cb6fd21422eb
Thirumoorthy369 <mr.thirumoorthys@gmail.com>
2025-06-09 12:22:34 +0530

    Time: 8 ms (43.82%) | Memory: 40.3 MB (80.11%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -5,3 +5,39 @@ class Solution {
         return l;
     }
 }
+
+/*
+TC : 0ms
+
+class Solution {
+      
+      int fibo (int n , int []dp)
+       {
+        if (n==1 || n==0)  return n;
+
+        if(dp[n]!=-1)
+           return dp[n];
+
+
+        dp[n]= fibo(n-1, dp)+fibo(n-2, dp);
+        return dp[n];
+       }
+     
+    public int fib(int n) {
+
+         int [] dp = new int[n+1];
+       for(int i=0; i<=n; i++){
+            dp[i] = -1;
+        }
+
+     return  fibo ( n , dp );
+
+      
+      
+   
+
+
+      
+    }
+}
+*/
