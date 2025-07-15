333eb173f4781647f093edd80ccc73aa5561520c
Thirumoorthy369 <mr.thirumoorthys@gmail.com>
2025-07-15 11:02:57 +0530

    Time: 16 ms (57.50%) | Memory: 45 MB (38.92%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,19 +1,16 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1]; 
+        Arrays.sort(coins);
+        int dp[]  = new int[amount +1];
 
-        Arrays.fill(dp, amount + 1);  
-           dp[0] = 0;
-
-        for (int i = 1; i <= amount; i++) {   
-            for (int coin : coins) {          
-
-                if (coin <= i) {             
-                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);  
-                }
+        for(int i = 1; i <= amount; i++){
+            dp[i] = amount+1;;
+            for(int coin : coins){
+                int t = i - coin;
+                if (t < 0) break;
+                dp[i] =  Math.min(dp[i] , dp[t]+1);
             }
         }
-
-        return (dp[amount] > amount) ? -1 : dp[amount]; 
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
