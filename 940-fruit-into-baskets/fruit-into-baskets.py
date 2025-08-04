5cc339cf992ed54291fd2d54ad575865b2a193c8
Thirumoorthy369 <mr.thirumoorthys@gmail.com>
2025-08-04 20:40:53 +0530

    Time: 100 ms (97.84%) | Memory: 17 MB (46.25%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def totalFruit(self, fruits):
+        left = point = total = 0
+        for i in range(1, len(fruits)):
+            if (fruits[i] != fruits[i-1] and fruits[i] != fruits[point-1]):
+                left = point
+            if (fruits[i] != fruits[i-1]):
+                point = i
+            total = max(total, i-left)
+        return total+1
+        """
+        :type fruits: List[int]
+        :rtype: int
+        """
+        
