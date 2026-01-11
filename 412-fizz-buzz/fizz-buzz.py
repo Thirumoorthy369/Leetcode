d9fa8cb84f57a021f994f4387d1a0b062a51626b
Thirumoorthy369 <mr.thirumoorthys@gmail.com>
2026-01-11 18:32:24 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (94.02%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -1,11 +1,6 @@
 class Solution(object):
     def fizzBuzz(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
         res = []
-
         for i in range(1, n+1):
             if i % 15 == 0:
                 res.append("FizzBuzz")
