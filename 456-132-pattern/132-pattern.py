9e39f09c02ba319f18911dee9357a16948dc29e6
Thirumoorthy369 <mr.thirumoorthys@gmail.com>
2026-03-06 15:24:01 +0530

    Time: 39 ms (94.96%) | Memory: 36.4 MB (89.63%) - LeetSync

--- 456-132-pattern/132-pattern.py
+++ 456-132-pattern/132-pattern.py
@@ -2,17 +2,12 @@ class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
         if len(nums) < 3:
             return False
-            
         stack = []
         second_max = float('-inf')
-        
         for num in reversed(nums):
             if num < second_max:
                 return True
-            
             while stack and num > stack[-1]:
                 second_max = stack.pop()
-            
             stack.append(num)
-        
         return False
